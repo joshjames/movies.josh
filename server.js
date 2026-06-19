6988658f438cb15f88f4e2197f29250a93db2b80
epicus <epic@la.apsau.net>
2026-06-19 15:59:34 +0000

    bug fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 console.log("!!! SERVER IS CURRENTLY INITIALIZING !!!");
 
 const express = require('express');
-const fs = require('fs');
+// const fs = require('fs');
 const path = require('path');
 const app = express();
 const PORT = 3000;
